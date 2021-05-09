dd56ed718c65a023d3a7f5da9fc52558d2369ab3
Aistis Olendra <aistis3211@gmail.com>
2021-05-09 23:16:34 +0300

    Rename Convertion to Converter, remove unused functions

---
+++ PVcase/Services/Converter.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using PVcase.Models;
+
+namespace PVcase.Services
+{
+    public class Converter
+    {
+        public List<Line> PointsToLines(List<Point> points)
+        {
+            var linesList = new List<Line>();
+
+            for (int i = 0; i < points.Count; ++i)
+            {
+                var start = points[i];
+                var end = new Point();
+
+                end = i != points.Count - 1 ? points[i + 1] : points[0];
+
+                var line = new Line(start, end);
+                linesList.Add(line);
+            }
+
+            return linesList;
+        }
+    }
+}
