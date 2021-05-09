5a330560efabeba2fe8eb8fe1e4e4c9f44cbdcdd
Aistis Olendra <aistis3211@gmail.com>
2021-05-09 17:59:57 +0300

    Add text parser class to parse inputs

---
+++ PVcase/Services/TextParser.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.RegularExpressions;
+
+namespace PVcase.Services
+{
+    public class TextParser
+    {
+        private readonly Regex _onlyInt = new Regex("[^0-9.-]+");
+        public bool IsTextInt(string text)
+        {
+            return _onlyInt.IsMatch(text);
+        }
+    }
+}
