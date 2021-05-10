e01b648f162ab0c507829c399ee9441098f83b87
Aistis Olendra <aistis3211@gmail.com>
2021-05-11 01:19:58 +0300

    Add ZoneCalculation tests

---
+++ UnitTests/ZoneCalculationsTests.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+using PVcase.Models;
+using PVcase.Services;
+
+namespace UnitTests
+{
+    public class ZoneCalculationsTests
+    {
+        [Test]
+        public void GetRange_InputPointList_ReturnsSiteCoordRange()
+        {
+            var zoneCalculations = new ZoneCalculations();
+            var coordinatesList = new List<Point>
+            {
+                new Point(4,5),
+                new Point(1,10),
+                new Point(6,15),
+                new Point(10,2)
+            };
+            var expected = new SiteCoordRange()
+            {
+                MaxX = 10,
+                MaxY = 15,
+                MinX = 1,
+                MinY = 2
+            };
+
+            var result = zoneCalculations.GetRange(coordinatesList);
+
+            Assert.AreEqual(expected,result);
+        }
+
+        [Test]
+        public void GetRange_InputNegativePointList_ReturnsSiteCoordRange()
+        {
+            var zoneCalculations = new ZoneCalculations();
+            var coordinatesList = new List<Point>
+            {
+                new Point(-4,5),
+                new Point(1,10),
+                new Point(6,-15),
+                new Point(-10,-2)
+            };
+            var expected = new SiteCoordRange()
+            {
+                MaxX = 6,
+                MaxY = 10,
+                MinX = -10,
+                MinY = -15
+            };
+
+            var result = zoneCalculations.GetRange(coordinatesList);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void GetRange_InputEmptyPointList_ReturnsNull()
+        {
+            var zoneCalculations = new ZoneCalculations();
+            var coordinatesList = new List<Point>();
+
+            var result = zoneCalculations.GetRange(coordinatesList);
+
+            Assert.AreEqual(null, result);
+        }
+    }
+}
