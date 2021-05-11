c4fcd06d2d0c70d4f12f876f97eb4db4f705b142
Aistis Olendra <aistis3211@gmail.com>
2021-05-11 15:22:49 +0300

    Add . to supported symbols, to allow type double inputs

--- PVcase/Services/TextParser.cs
+++ PVcase/Services/TextParser.cs
@@ -4,7 +4,7 @@ namespace PVcase.Services
 {
     public class TextParser
     {
-        private readonly Regex _onlyInt = new Regex("[^0-9]+");
+        private readonly Regex _onlyInt = new Regex("[^0-9.]+");
         public bool IsTextInt(string text)
         {
             return _onlyInt.IsMatch(text);
